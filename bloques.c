e49f30db526552bea648239ba329956aad994a97
cxz03 <cxz03@debian.Home>
2025-02-25 16:51:39 +0100

    implementado ficheros_basico.h y formateo de comentarios

--- bloques.c
+++ bloques.c
@@ -1,12 +1,11 @@
-// bloques.c
 #include "bloques.h"
+
 static int descriptor = 0;
 
-
-//Montar el fichero
+// Montar el fichero
 int bmount(const char *camino){
     
-    if ((descriptor = open(camino, O_RDWR | O_CREAT,0666)) == -1) // Abrimos el fichero y controlamos que no se produzcan errores
+    if ((descriptor = open(camino, O_RDWR | O_CREAT,0666)) == -1)   // Abrimos el fichero y controlamos que no se produzcan errores
     {
         perror(RED "Error: bmount(), open() == -1");
         printf(RESET);
@@ -17,7 +16,7 @@ int bmount(const char *camino){
     return descriptor;
 }
 
-//Hace un close del archivo
+// Hace un close del archivo
 int bumount(){
     if(close(descriptor)==-1){
         perror(RED "Error: bumount, close() == -1");
@@ -27,10 +26,10 @@ int bumount(){
     return EXITO;
 }
 
-//Escribe 1 bloque en el dispositivo virtual, devuelve BLOCKSIZE si ha ido bien o -1 si no ha ido bien
+// Escribe 1 bloque en el dispositivo virtual, devuelve BLOCKSIZE si ha ido bien o -1 si no ha ido bien
 int bwrite(unsigned int nbloque, const void *buf){
     int bytesEscritos;
-    //Nos posicionamos: (descriptor apuntará a la posición deseada)
+    // Nos posicionamos: (descriptor apuntará a la posición deseada)
     if((lseek(descriptor, nbloque*BLOCKSIZE,  SEEK_SET)) == -1){
         perror(RED "Error: bwrite(), lseek() == -1");
         printf(RESET);
@@ -45,7 +44,7 @@ int bwrite(unsigned int nbloque, const void *buf){
     return bytesEscritos;
 }
 
-//Lee un bloque (como bwrite pero leyendo)
+// Lee un bloque (como bwrite pero leyendo)
 int bread(unsigned int nbloque, void *buf){
     int bytesLeidos;
     if((lseek(descriptor, nbloque*BLOCKSIZE,  SEEK_SET)) == -1){
