8a77bb811d57da797a97454f498387248486a970
cxz03 <chengxz0393@gmail.com>
2025-03-17 15:44:57 +0100

    start escribir.c

--- escribir.c
+++ escribir.c
@@ -1,1 +1,15 @@
 #include "ficheros.h"
+
+#define DEBUGN5 true
+
+int main(int argc, char **argv) {
+    #if DEBUGN5
+    int nInodoReservado = reservar_inodo('f', 6);
+    if (nInodoReservado == FALLO) {
+        perror(RED "Error: permitir.c -> main() -> reservar_inodo() == FALLO");
+        printf(RESET);
+        return FALLO;
+    }
+    
+    #endif
+}
